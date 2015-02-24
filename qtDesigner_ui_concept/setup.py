e2d0067f90b349b9b1fc0cb2a089b27f6791c7a0
Jaafar Bouayad <bouayad.jaafar@gmail.com>
2015-02-24 18:49:07 +0100

    Cleanup

--- qtDesigner_ui_concept/setup.py
+++
@@ -1,8 +0,0 @@
-import sys
-from cx_Freeze import setup, Executable
-
-setup(
-    name = "Jozam",
-    version = "0",
-    description = "Task Manager",
-    executables = [Executable("qSplitter.py")])
